28cd9b2706adea5902ab7d5a97b895a8d2272fa9
Tamizhselvanr <rvtamizhselvan@gmail.com>
2019-07-30 22:01:11 +0530

    paranthesis

--- paranthesis-problem.py
+++ paranthesis-problem.py
@@ -1,19 +1,39 @@
-a = "(){[]}("
+'''
+
+Ques 2: Given a string of brackets (, ), {, }, [, ], find the position in the string where the orders of brackets breaks.
+
+I/p: ())
+
+O/p: 3
+
+
+
+I/p: (){[]}(
+
+O/p: 8
+
+https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
+
+'''
+wrong = 0
+a = '(((){[}}'
 stac = []
-for i in a:
-    if (i == '(' or i == '[' or i == '{'):
-        if(a.index(i) != len(a) - 1):
-            stac.append(i)
+for i in range(len(a)):
+    if (a[i] == '(' or a[i] == '[' or a[i] == '{'):
+        if(i == len(a) - 1):
+            wrong = i+1
         else:
-            thappu = a.index(i)
-    elif (i == ')' or i == '}' or i == ']'):
+            stac.append(a[i])
+    elif (a[i] == ')' or a[i] == '}' or a[i] == ']'):
         if(len(stac) != 0):
             stac.pop()
 
         else:
-            thappu = a.index(i)
+            wrong = i
 print(stac)
-print("thappu",thappu+1)
+if(wrong == 0):
+    wrong = len(a) - 1
+print("",wrong+1)
 
 
 
--- prefix-suffix.py
+++ prefix-suffix.py
@@ -3,6 +3,7 @@
 Ques 1: Given a string, find the longest length of a prefix which is also a suffix.
 
 https://www.geeksforgeeks.org/longest-prefix-also-suffix/
+
 '''
 
 a = "cdcdcdc"
@@ -12,7 +13,7 @@ count = 0
 while(i < len(a) and j >= 0):
     print(a[0:i],a[j:])
     if(a[0:i] == a[j:]):
-        count = i
+        count = i+1
     i += 1
     j -= 1
 print(count)
